7cac80d2bb66e45d1563f1dda46ab662987e01d6
Green-Avocado <greenavocado@protonmail.com>
2020-12-01 13:33:32 -0800

    feat: reorganised files, added makefile

--- src/01/part1.c
+++ src/01/part1.c
@@ -5,20 +5,29 @@ int main() {
     FILE *input;
     input = fopen("./input", "r");
 
+    if(input == NULL) {
+        puts("Error: could not read input file");
+        return 1;
+    }
+
     int num;
 
     int size = 64 * sizeof(int);
     int i = 0;
     int *operands = (int *) malloc(size);
 
-    while (fscanf(input, "%d\n", &num)) {
-        if(i == size) {
+    while (fscanf(input, "%d\n", &num))
+    {
+        if(i == size)
+        {
             size += 64 * sizeof(int);
             operands = (int *) realloc(operands, size);
         }
 
-        for(int ii = 0; ii < i; ii++) {
-            if(*(operands + ii) + num == 2020) {
+        for(int ii = 0; ii < i; ii++)
+        {
+            if(*(operands + ii) + num == 2020)
+            {
                 printf("%d\n", *(operands + ii) * num);
 
                 free(operands);
