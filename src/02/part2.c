67dd9ad71c5087d0660bb37cc305b4013292a777
Green-Avocado <greenavocado@protonmail.com>
2020-12-02 10:32:51 -0800

    feat: solved part 2

---
+++ src/02/part2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int checkline(int a, int b, char c, char *pass, size_t passlen)
+{
+    if((*(pass + a - 1) == c) != (*(pass + b - 1) == c))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    FILE *input;
+    input = fopen("./input", "r");
+
+    char line[256];
+
+    int valid = 0;
+
+    if(input == NULL)
+    {
+        puts("Error: could not read input file");
+        return 1;
+    }
+
+    while(fgets(line, sizeof(line), input) != NULL)
+    {
+        int a, b;
+        char c;
+        char pass[256];
+
+        sscanf(line, "%d-%d %c: %s\n", &a, &b, &c, pass);
+
+        valid += checkline(a, b, c, pass, sizeof(pass));
+    }
+
+    printf("%d\n", valid);
+
+    return 0;
+}
+
