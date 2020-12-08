4d6697f733e25992fae3d4c31df20bec9cff3ade
Green-Avocado <greenavocado@protonmail.com>
2020-12-07 22:09:38 -0800

    feat: solved part 1

--- src/08/part1.c
+++ src/08/part1.c
@@ -1,4 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
+struct OpCode
+{
+    char op[4];
+    int arg;
+    int called;
+};
+
+int runOp(struct OpCode *op, int *instruction, int *accumulator)
+{
+    if(op -> called != 0)
+    {
+        return 1;
+    }
+
+    op -> called += 1;
+
+    if(strncmp(op -> op, "nop", 3) == 0)
+    {
+        (*instruction)++;
+    }
+    else if(strncmp(op -> op, "jmp", 3) == 0)
+    {
+        *instruction += op -> arg;
+    }
+    else if(strncmp(op -> op, "acc", 3) == 0)
+    {
+        *accumulator += op -> arg;
+        (*instruction)++;
+    }
+
+    return 0;
+}
 
 int main()
 {
@@ -12,15 +46,30 @@ int main()
     }
 
     char line[1024];
-    int result = 0;
+
+    struct OpCode ops[1024];
+    int numOfOps = 0;
 
     while(fgets(line, sizeof(line), input) != NULL)
     {
+        sscanf(line, "%s %d", ops[numOfOps].op, &ops[numOfOps].arg);
+        ops[numOfOps].called = 0;
+
+        numOfOps++;
     }
 
-    result = numOfTargets - 1;
+    int instruction = 0;
+    int accumulator = 0;
 
-    printf("%d\n", result);
+    while(instruction < numOfOps)
+    {
+        if(runOp(ops + instruction, &instruction, &accumulator) != 0)
+        {
+            break;
+        }
+    }
+
+    printf("%d\n", accumulator);
 
     return 0;
 }
