8debe41c33ab78091cb7df4aafed1f7cd121e04a
Green-Avocado <greenavocado@protonmail.com>
2020-12-17 22:31:03 -0800

    feat: solved part 1

--- src/17/part1.c
+++ src/17/part1.c
@@ -1,4 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int getPos(int max_size, int x, int y, int z)
+{
+    return (x) + (y * max_size) + (z * max_size * max_size);
+}
+
+int getNewState(int max_size, int size, int *grid, int x, int y, int z)
+{
+    int activeNear = 0;
+
+    for(int xx = x-1; xx <= x+1; xx++)
+    {
+        for(int yy = y-1; yy <= x+1; yy++)
+        {
+            for(int zz = z-1; zz <= z+1; zz++)
+            {
+                if(
+                        (xx > 0 && xx < size) &&
+                        (yy > 0 && yy < size) &&
+                        (zz > 0 && zz < size)
+                  )
+                {
+                    activeNear += grid[getPos(max_size, xx, yy, zz)];
+                }
+            }
+        }
+    }
+
+    int currentState = grid[getPos(max_size, x, y, z)];
+
+    if(
+            (currentState == 1 && activeNear == 2) ||
+            (activeNear == 3)
+      )
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int countActive(int max_size, int *grid)
+{
+    int active = 0;
+
+    for(int i = 0; i < max_size * max_size * max_size; i++)
+    {
+        active += grid[i];
+    }
+
+    return active;
+}
 
 int main()
 {
@@ -11,15 +66,27 @@ int main()
         return 1;
     }
 
-    char line[1024];
-    int length = 0;
+    char line[16];
+    int max_size = 16;
+    int size = 0;
+    int *grid = (int*) calloc(16 * 16 * 16, sizeof(int));
 
     while(fgets(line, sizeof(line), input) != NULL)
     {
-        length++;
+        int i = 0;
+        while(line[i] == '.' || line[i] == '#')
+        {
+            if(line[i] == '#')
+            {
+                grid[getPos(max_size, i, size, 0)] = '1';
+            }
+            
+            i++;
+        }
+        size++;
     }
 
-    printf("%d\n", length);
+    printf("%d\n", countActive(max_size, grid));
 
     return 0;
 }
--- src/18/part1.c
+++ src/18/part1.c
@@ -1,5 +1,65 @@
 #include <stdio.h>
 
+unsigned long long int eval(char *eq, int *pickup)
+{
+    unsigned long long int result = 0;
+    int i = 0;
+    char op = '+';
+
+    while(eq[i] != ')' && eq[i] != '\0' && eq[i] != '\n')
+    {
+        if(eq[i] == '(')
+        {
+            unsigned long long int arg;
+            int shift;
+
+            arg = eval(eq + i + 1, &shift);
+
+            if(op == '+')
+            {
+                result += arg;
+            }
+            else
+            {
+                result *= arg;
+            }
+
+            i += shift + 1;
+        }
+        else if(eq[i] == '*' || eq[i] == '+')
+        {
+            op = eq[i];
+        }
+        else if(eq[i] != ' ')
+        {
+            unsigned int arg;
+            int shift;
+
+            sscanf(eq + i, "%u%n", &arg, &shift);
+
+            if(op == '+')
+            {
+                result += arg;
+            }
+            else
+            {
+                result *= arg;
+            }
+
+            i += shift - 1;
+        }
+
+        i++;
+    }
+
+    if(pickup != NULL)
+    {
+        *pickup = i;
+    }
+
+    return result;
+}
+
 int main()
 {
     FILE *input;
@@ -12,14 +72,14 @@ int main()
     }
 
     char line[256];
-    int length = 0;
+    unsigned long long int sum = 0;
 
     while(fgets(line, sizeof(line), input) != NULL)
     {
-        length++;
+        sum += eval(line, NULL);
     }
 
-    printf("%d\n", length);
+    printf("%llu\n", sum);
 
     return 0;
 }
