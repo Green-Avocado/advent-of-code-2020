19b4138e0842edc74920171eb8f6e98ad49c0d52
Green-Avocado <greenavocado@protonmail.com>
2020-12-18 23:27:17 -0800

    feat: read and store input

--- src/19/part1.c
+++ src/19/part1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+struct Rule
+{
+    int id;
+    int type;
+    char c;
+
+    int rule1A;
+    int rule1B;
+    int rule2A;
+    int rule2B;
+};
+
 int main()
 {
     FILE *input;
@@ -11,12 +23,58 @@ int main()
         return 1;
     }
 
-    char line[1024];
+    char line[128];
+    int readingPhase = 0;
     int length = 0;
+
+    struct Rule rules[256];
+    char messages[1024][128];
 
     while(fgets(line, sizeof(line), input) != NULL)
     {
-        length++;
+        if(line[0] == '\n')
+        {
+            length = 0;
+        }
+        else
+        {
+            if(readingPhase = 0)
+            {
+                int index1;
+
+                sscanf(line, "%d: %n", &(rules[length].id), &index1);
+
+                if(line[index1] == '"')
+                {
+                    rules[length].type = 0;
+                    rules[length].c = line[index1 + 1];
+                }
+                else
+                {
+                    int index2;
+
+                    sscanf(line + index1, "%d %d%n", rules[length].rule1A, rules[length].rule1B);
+
+                    index2 += index1;
+
+                    if(line[index2] == '\0')
+                    {
+                        rules[length].type = 1;
+                    }
+                    else
+                    {
+                        rules[length].type = 2;
+                        sscanf(line + index2, " | %d %d%n", rules[length].rule2A, rules[length].rule2B);
+                    }
+                }
+            }
+            else
+            {
+                sscanf(line, "%s", messages + length);
+            }
+
+            length++;
+        }
     }
 
     printf("%d\n", length);
