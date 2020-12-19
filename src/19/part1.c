a8df4dccd21d5deefc71936fe0b1bb40d9ea60e6
Green-Avocado <greenavocado@protonmail.com>
2020-12-19 02:12:36 -0800

    fix: removed erroneous implementation

--- src/19/part1.c
+++ src/19/part1.c
@@ -6,11 +6,66 @@ struct Rule
     int type;
     char c;
 
-    int rule1A;
-    int rule1B;
-    int rule2A;
-    int rule2B;
+    int subs[8][8];
+    int subs_len;
+    int subs_subs_len;
 };
+
+void merge(struct Rule *out, struct Rule *arr1, int len1, struct Rule *arr2, int len2)
+{
+    int ind1 = 0;
+    int ind2 = 0;
+
+    for(int i = 0; i < len1 + len2; i++)
+    {
+        if(ind2 >= len2 || (ind1 < len1 && arr1[ind1].id < arr2[ind2].id))
+        {
+            out[i] = arr1[ind1];
+            ind1++;
+        }
+        else
+        {
+            out[i] = arr2[ind2];
+            ind2++;
+        }
+    }
+}
+
+void sort(struct Rule *arr, int len)
+{
+    if(len > 1)
+    {
+        int len1 = len / 2;
+        int len2 = len - len1;
+
+        struct Rule arr1[len1];
+        struct Rule arr2[len2];
+
+        for(int i = 0; i < len; i++)
+        {
+            if(i < len1)
+            {
+                arr1[i] = arr[i];
+            }
+            else
+            {
+                arr2[i - len1] = arr[i];
+            }
+        }
+
+        sort(arr1, len1);
+        sort(arr2, len2);
+
+        merge(arr, arr1, len1, arr2, len2);
+    }
+}
+
+int validate(char *message, struct Rule *rules, int rules_length)
+{
+    int valid = 1;
+
+    return valid;
+}
 
 int main()
 {
@@ -25,59 +80,53 @@ int main()
 
     char line[128];
     int readingPhase = 0;
-    int length = 0;
 
     struct Rule rules[256];
+    int rules_length = 0;
+
     char messages[1024][128];
+    int messages_length = 0;
 
     while(fgets(line, sizeof(line), input) != NULL)
     {
         if(line[0] == '\n')
         {
-            length = 0;
+            readingPhase = 1;
         }
-        else
+        else if(readingPhase == 0)
         {
-            if(readingPhase = 0)
+            int index1;
+
+            sscanf(line, "%d: %n", &(rules[rules_length].id), &index1);
+
+            if(line[index1] == '"')
             {
-                int index1;
-
-                sscanf(line, "%d: %n", &(rules[length].id), &index1);
-
-                if(line[index1] == '"')
-                {
-                    rules[length].type = 0;
-                    rules[length].c = line[index1 + 1];
-                }
-                else
-                {
-                    int index2;
-
-                    sscanf(line + index1, "%d %d%n", rules[length].rule1A, rules[length].rule1B);
-
-                    index2 += index1;
-
-                    if(line[index2] == '\0')
-                    {
-                        rules[length].type = 1;
-                    }
-                    else
-                    {
-                        rules[length].type = 2;
-                        sscanf(line + index2, " | %d %d%n", rules[length].rule2A, rules[length].rule2B);
-                    }
-                }
+                rules[rules_length].type = 0;
+                rules[rules_length].c = line[index1 + 1];
             }
             else
             {
-                sscanf(line, "%s", messages + length);
             }
 
-            length++;
+            rules_length++;
+        }
+        else
+        {
+            sscanf(line, "%s", messages + messages_length);
+            messages_length++;
         }
     }
 
-    printf("%d\n", length);
+    sort(rules, rules_length);
+
+    int valid = 0;
+
+    for(int i = 0; i < messages_length; i++)
+    {
+        valid += validate(messages[i], rules, rules_length);
+    }
+
+    printf("%d\n", valid);
 
     return 0;
 }
